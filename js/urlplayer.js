bbd22ef2fdb567417906cf19058a8e1b3942046e
Tobias Bertelsen <tobias.bertelsen@gmail.com>
2014-09-01 21:46:06 +0200

    Enableling url to be populated via URL
    Enabling the url input field to be populated via an URL argument 'url'
    
    Author: Tobias Bertelsen <tobias.bertelsen@gmail.com>
    Modifier & Committer: Vignesh Venkat <vigneshacitizenofindia@gmail.com>

--- js/urlplayer.js
+++ js/urlplayer.js
@@ -3,7 +3,23 @@ var currentUrl = '';
 
 $(function() {
   player = new CastPlayer();
+  var url = getUrlParameter("url")
+  if (url) {
+    $('#url').val(url);
+  }
 });
+
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+      return decodeURIComponent(sParameterName[1]);
+    }
+  }
+}
+
 
 function launchApp() {
   player.launchApp();
